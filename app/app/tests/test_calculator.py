eb3ae971b5f30fd06ce4515fa3cb1848cbdabdbf
Jose Miguel Gomes <jose_miguel_gomes@hotmail.com>
2022-05-27 16:43:01 +0100

    Add Unit Test Example (TDD)

---
+++ app/app/tests/test_calculator.py
@@ -0,0 +1,24 @@
+"""
+Unit tests for calc
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalculatorTests(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_positive_numbers(self):
+        res = calc.add(1, 3)
+        self.assertEqual(res, 4)
+
+    def test_add_negative_numbers(self):
+        res = calc.add(-1, -3)
+        self.assertEqual(res, -4)
+
+    def test_add_positive_negative_numbers(self):
+        res = calc.add(3, -1)
+        self.assertEqual(res, 2)
+
+        
+    
